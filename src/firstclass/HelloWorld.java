317f05195c13c4d555ef71036711adc229559bf9
Askar Musakunov <askarmusakunov@Askars-MacBook-Pro.local>
2020-11-16 21:25:56 -0600

    first commit

---
+++ src/firstclass/HelloWorld.java
@@ -0,0 +1,9 @@
+package firstclass;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Askar");
+        System.out.println("Yay! I am soon SDET to be");
+    }
+}
